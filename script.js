40a2a1fa7a16c0b48ffcca2cb4336567da7166f1
youngeunkim <pkhj286@gmail.com>
2023-04-15 13:46:03 +0900

    ADD : 인스타 메인화면

---
+++ script.js
@@ -0,0 +1,16 @@
+const transform = document.querySelector(".change-name");
+const profilename = document.querySelector(".transform-profile-name");
+function onhandleclick(){
+    const change = prompt("아이디를 입력하세용");
+    
+    if(change==""){
+        alert("값 입력 안됐는데요 --");
+    }
+    else{
+        profilename.innerText=change; //profilename의 태그에 change의 값을 넣어줌
+    }
+}
+    
+
+
+transform.addEventListener("click", onhandleclick); //transform에 이벤트가 일어났을 때
